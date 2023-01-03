7f7550d7ff66c2a439aa75dbabef59da0c685313
mpjcoomes <mpjcoomes@gmail.com>
2023-01-03 17:42:24 +1100

    Update 99BottlesOfBeer.py

--- puzzles/99BottlesOfBeer.py
+++ puzzles/99BottlesOfBeer.py
@@ -2,6 +2,9 @@ a = "bottles of beer"
 b = "on the wall"
 c = "Take one down, pass it around"
 
+
+
+
 for i in range(99, 0, -1):
     if i > 2:
         print(i, a, b, "\n" + str(i), a, "\n" + c, "\n" + str(i - 1), a, b, "\n")
